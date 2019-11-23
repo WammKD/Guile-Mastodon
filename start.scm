bdca98498507d0d1e7c95d7b424f330501304b9b
Sherlock Holmes <jaft.r@outlook.com>
2019-11-22 20:53:48 -0600

    It's a beginning; all the modules I'll likely need and, commented out, the setup for creating a module of these.

---
+++ start.scm
@@ -0,0 +1,20 @@
+;; (define-module ()
+;;   #:use-module (ice-9 receive)
+;;   #:use-module (json)
+;;   #:use-module (rnrs bytevectors)
+;;   #:use-module (srfi srfi-9)
+;;   #:use-module (srfi srfi-26)
+;;   #:use-module (web client)
+;;   #:export (<mastodon-instance-application>
+;;             masto-instance-app?
+;;             masto-app-domain
+;;             masto-app-key
+;;             masto-app-secret
+;;             masto-app-token
+;;             masto-app-name
+;;             masto-app-redirect
+;;             masto-app-scopes
+;;             masto-app-website
+;;             instantiate-masto-app))
+(use-modules (srfi srfi-9)  (web   client)  (json)
+             (srfi srfi-26) (ice-9 receive) (rnrs bytevectors))

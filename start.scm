7b6a55f60d477e2d1e4460f3205bcd809b43deec
Sherlock Holmes <jaft.r@outlook.com>
2019-11-23 22:42:27 -0600

    Might as well implement this part, seeing as I'm just copying the functions into the REPL directly (anyway).

--- start.scm
+++ start.scm
@@ -1,22 +1,24 @@
-;; (define-module ()
-;;   #:use-module (ice-9 receive)
-;;   #:use-module (json)
-;;   #:use-module (rnrs bytevectors)
-;;   #:use-module (srfi srfi-9)
-;;   #:use-module (srfi srfi-26)
-;;   #:use-module (web client)
-;;   #:export (<mastodon-instance-application>
-;;             masto-instance-app?
-;;             masto-app-domain
-;;             masto-app-key
-;;             masto-app-secret
-;;             masto-app-name
-;;             masto-app-redirect
-;;             masto-app-scopes
-;;             masto-app-website
-;;             instantiate-masto-app))
-(use-modules (srfi srfi-9)  (web   client)  (json)
-             (srfi srfi-26) (ice-9 receive) (rnrs bytevectors))
+(define-module (elefan auth)
+  #:use-module (ice-9 receive)
+  #:use-module (json)
+  #:use-module (rnrs bytevectors)
+  #:use-module (srfi srfi-9)
+  #:use-module (srfi srfi-26)
+  #:use-module (web client)
+  #:export (<mastodon-instance-application>
+            masto-instance-app?
+            masto-app-domain
+            masto-app-key
+            masto-app-secret
+            masto-app-name
+            masto-app-redirect
+            masto-app-scopes
+            masto-app-website
+            masto-app-instantiate
+            masto-app-authorize-uri
+            masto-app-token-via-code
+            masto-app-token-via-user-cred
+            masto-app-token-via-client-cred))
 
 (define (assemble-params params)
   (string-append/shared "?" (string-join (map (cut string-join <> "=") params) "&")))

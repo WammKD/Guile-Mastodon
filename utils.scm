2e628ad084bf530460472de39baeeeb7276a5b5a
Sherlock Holmes <jaft.r@outlook.com>
2019-11-24 23:02:07 -0600

    I always found these two macros useful.

--- utils.scm
+++ utils.scm
@@ -1,6 +1,46 @@
 (define-module (elefan utils)
   #:use-module (srfi srfi-26)
-  #:export (assemble-params))
+  #:export (assemble-params
+            if-let
+            if-let-helper))
+
+(define-syntax if-let-helper
+  (syntax-rules ()
+    [(_ letVersion
+        ([bnd             val]    ...)
+        (cnd                      ...)
+        ()                             then else) (letVersion ([bnd val] ...)
+                                                    (if (and cnd ...) then else))]
+    [(_ letVersion
+        ([bnd             val]    ...)
+        (cnd                      ...)
+        ([binding       value] . rest) then else) (if-let-helper letVersion
+                                                                 ([bnd val] ... [binding value])
+                                                                 (cnd                       ...)
+                                                                 rest                            then else)]
+    [(_ letVersion
+        ([bnd             val]    ...)
+        (cnd                      ...)
+        ([binding funct value] . rest) then else) (if-let-helper letVersion
+                                                                 ([bnd val] ... [binding value])
+                                                                 (cnd       ... (funct binding))
+                                                                 rest                            then else)]))
+(define-syntax if-let
+  (syntax-rules ()
+    [(_ ([binding         value]  ...) then else) (let ([binding value] ...)
+                                                    (if (and binding ...) then else))]
+    [(_ (binding-funct-value      ...) then else) (if-let-helper let
+                                                                 ()
+                                                                 ()
+                                                                 (binding-funct-value ...) then else)]))
+(define-syntax if-let*
+  (syntax-rules ()
+    [(_ ([binding         value]  ...) then else) (let* ([binding value] ...)
+                                                    (if (and binding ...) then else))]
+    [(_ (binding-funct-value      ...) then else) (if-let-helper let*
+                                                                 ()
+                                                                 ()
+                                                                 (binding-funct-value ...) then else)]))
 
 (define (assemble-params params)
   (string-append/shared "?" (string-join (map (cut string-join <> "=") params) "&")))

b3551a73666d0e1024ce0158d96991b091eb9af4
Sherlock Holmes <jaft.r@outlook.com>
2019-11-24 23:09:56 -0600

    All the endpoints regarding blocks.

---
+++ blocks.scm
@@ -0,0 +1,40 @@
+(define-module (elefan blocks)
+  #:use-module (elefan auth)
+  #:use-module (elefan entities)
+  #:use-module (ice-9 receive)
+  #:use-module (json)
+  #:use-module (rnrs bytevectors)
+  #:use-module (web client)
+  #:export (masto-blocks-all
+            masto-block-account
+            masto-unblock-account))
+
+(define* (masto-blocks-all mastoApp #:optional [limit 40])
+  (receive (header body)
+      (http-get
+        (string-append (masto-app-domain mastoApp) "/api/v1/blocks"
+                       "?limit="                   (number->string limit))
+        #:headers `((Authorization . ,(string-append
+                                        "Bearer "
+                                        (masto-app-token mastoApp)))))
+    (generate-masto-account-array (json-string->scm (utf8->string body)))))
+
+(define (masto-block-account mastoApp accountID)
+  (receive (header body)
+      (http-post
+        (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
+                       accountID                   "/block")
+        #:headers `((Authorization . ,(string-append
+                                        "Bearer "
+                                        (masto-app-token mastoApp)))))
+    (generate-masto-relationship (json-string->scm (utf8->string body)))))
+
+(define (masto-unblock-account mastoApp accountID)
+  (receive (header body)
+      (http-post
+        (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
+                       accountID                   "/unblock")
+        #:headers `((Authorization . ,(string-append
+                                        "Bearer "
+                                        (masto-app-token mastoApp)))))
+    (generate-masto-relationship (json-string->scm (utf8->string body)))))

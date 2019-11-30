17a9a899a02b066c0e2e97d1f7c97d39ee4321d8
Sherlock Holmes <jaft.r@outlook.com>
2019-11-29 21:11:34 -0600

    Perhaps a simple `symbol->string` would do but this allows me to not have to worry about what I'm handling, no matter what state it's in (from one of my own record objects or coming from a JSON Scheme object); I suppose less type secure but I can live with that.

--- enums.scm
+++ enums.scm
@@ -1,8 +1,9 @@
 (define-module (elefan enums)
   #:use-module (elefan utils)
-  #:export (enum-value-of  enum-member?
-            enum-elem=?    ATTACHMENT_TYPE_ENUM
-            CARD_TYPE_ENUM FILTER_CONTEXT_ENUM  STATUS_VISIBILITY_ENUM))
+  #:export (enum-value-of        enum-member?
+            enum-elem=?          enum-member-or-value->string
+            ATTACHMENT_TYPE_ENUM         CARD_TYPE_ENUM
+             FILTER_CONTEXT_ENUM STATUS_VISIBILITY_ENUM))
 
 (define (enum-value-of value enum)
   (if-let ([possMem (memq (string->symbol value) enum)]) (car possMem) possMem))
@@ -14,6 +15,9 @@
   (eq?
     ((if (string? elemOrValue1) string->symbol identity) elemOrValue1)
     ((if (string? elemOrValue2) string->symbol identity) elemOrValue2)))
+
+(define (enum-member-or-value->string member-or-value)
+  ((if (string? member-or-value) identity symbol->string) member-or-value))
 
 
 

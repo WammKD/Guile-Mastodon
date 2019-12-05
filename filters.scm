89f7f59bc15487ccd413dd7194f6fbe673c10c9a
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 21:44:43 -0600

    Update filters…
    I realized that "expires_in" takes an amount of _seconds_, rather than
    a date…, so I had to update that section, as well.
    
    While I don't know that you would necessarily ever be able to
    organically end up at this spot, – since I want the user to be able to
    pass a filter record object to `masto-filter-create`, if so desired –
    the user could store a date object in the `expiresAt` spot.
    
    The function will then just do the math for them and calculate the
    number of second between now and when the date it should expire at is.
    
    It will also handle a time object or just the number of
    seconds. This'll provide full support to any kind of reasonable object
    types that might be used to represent this data.

--- filters.scm
+++ filters.scm
@@ -3,29 +3,20 @@
   #:use-module (elefan entities)
   #:use-module (elefan enums)
   #:use-module (elefan utils)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
   #:use-module (srfi srfi-19)
-  #:use-module (srfi srfi-26)
-  #:use-module (web client)
-  #:export (masto-filters-all masto-filter-status masto-unfilter-status))
+  #:export (masto-filters-all masto-filter-create
+            masto-filter-get  masto-filter-update masto-filter-delete))
 
 (define (masto-filters-all mastoApp)
-  (receive (header body)
-      (http-get (string-append (masto-app-domain mastoApp) "/api/v1/filters")
-                #:headers `((Authorization . ,(string-append
-                                                "Bearer "
-                                                (masto-app-token mastoApp)))))
-    (generate-masto-filter (json-string->scm (utf8->string body)))))
+  (generate-masto-filter
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/filters")
+      #:token (masto-app-token mastoApp))))
 
 (define* (masto-filter-create mastoApp #:key filter    phrase       context
                                              expiresIn irreversible wholeWord)
-  (let ([f (if filter filter `(("phrase"       . ,phrase)
-                               ("context"      . ,context)
-                               ("expires_at"   . ,expiresIn)
-                               ("irreversible" . ,irreversible)
-                               ("whole_word"   . ,wholeWord)))])
+  (let ([f (if filter filter (make-masto-filter #f        phrase       context
+                                                expiresIn irreversible wholeWord))])
     (cond
      [(not (enum-member? (masto-filter-context f) FILTER_CONTEXT_ENUM))
            (error (string-append
@@ -37,45 +28,42 @@
                     "ERROR: In procedure masto-filter-create:\n"
                     "In procedure masto-filter-create: No phrase "
                     "provided; filter phrase is required."))]
-     [else (receive (header body)
-               (http-post
-                 (string-append (masto-app-domain mastoApp) "/api/v1/filters"
-                                (assemble-params
-                                  `(("phrase"       ,(masto-filter-phrase f))
-                                    ("context"      ,(enum-member-or-value->string
-                                                       (masto-filter-context f)))
-                                    ("irreversible" ,(if (masto-filter-irreversible f)
-                                                         "true"
-                                                       "false"))
-                                    ("whole_word"   ,(if (masto-filter-whole-word   f)
-                                                         "true"
-                                                       "false"))))
-                                (if-let ([ei not (masto-filter-expires-at f)])
-                                    ""
-                                  (string-append
-                                    "&expires_in="
-                                    (if (string? ei)
-                                        ei
-                                      (date->string ei "~Y-~m-~dT~H:~M:~S.~N~z")))))
-                 #:headers `((Authorization . ,(string-append
-                                                 "Bearer "
-                                                 (masto-app-token mastoApp)))))
-             (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                            (utf8->string body))])
-                 (error (assoc-ref bodySCM "error"))
-               (generate-masto-filter bodySCM)))])))
+     [else (generate-masto-filter
+             (http 'post
+               (string-append (masto-app-domain mastoApp) "/api/v1/filters")
+               #:token       (masto-app-token mastoApp)
+               #:queryParams `(("phrase"       ,(masto-filter-phrase f))
+                               ("context"      ,(enum-member-or-value->string
+                                                  (masto-filter-context f)))
+                               ("irreversible" ,(boolean->string
+                                                  (masto-filter-irreversible f)))
+                               ("whole_word"   ,(boolean->string
+                                                  (masto-filter-whole-word   f)))
+                               ("expires_in"   ,(if-let ([ei not (masto-filter-expires-at f)])
+                                                    ei
+                                                  (number->string
+                                                    (cond
+                                                     [(date? ei)
+                                                           (time-second
+                                                             (time-difference
+                                                               (date->time-utc ei)
+                                                               (date->time-utc (current-date
+                                                                                 (date-zone-offset ei)))))]
+                                                     [(time? ei)
+                                                           (time-second ei)]
+                                                     [(number? ei)
+                                                           ei]
+                                                     [else (error (string-append
+                                                                    "ERROR: In procedure masto-filter-create:\n"
+                                                                    "In procedure masto-filter-create: "
+                                                                    "expiresIn must be srfi-19 date or time "
+                                                                    "or number of seconds"))])))))))])))
 
 (define (masto-filter-get mastoApp filterID)
-  (receive (header body)
-      (http-get
-        (string-append (masto-app-domain mastoApp) "/api/v1/filters/" filterID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-filter bodySCM))))
+  (generate-masto-filter
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/filters/" filterID)
+      #:token (masto-app-token mastoApp))))
 
 (define* (masto-filter-update mastoApp #:key filter  id        phrase
                                              context expiresIn irreversible wholeWord)
@@ -101,42 +89,41 @@
                     "ERROR: In procedure masto-filter-update:\n"
                     "In procedure masto-filter-update: No phrase "
                     "provided; filter id is required."))]
-     [else (receive (header body)
-               (http-put
-                 (string-append (masto-app-domain mastoApp) "/api/v1/filters/"
-                                (masto-filter-id         f)
-                                (assemble-params
-                                  `(("phrase"       ,(masto-filter-phrase f))
-                                    ("context"      ,(enum-member-or-value->string
-                                                       (masto-filter-context f)))
-                                    ("irreversible" ,(if (masto-filter-irreversible f)
-                                                         "true"
-                                                       "false"))
-                                    ("whole_word"   ,(if (masto-filter-whole-word   f)
-                                                         "true"
-                                                       "false"))))
-                                (if-let ([ei not (masto-filter-expires-at f)])
-                                    ""
-                                  (string-append
-                                    "&expires_in="
-                                    (if (string? ei)
-                                        ei
-                                      (date->string ei "~Y-~m-~dT~H:~M:~S.~N~z")))))
-                 #:headers `((Authorization . ,(string-append
-                                                 "Bearer "
-                                                 (masto-app-token mastoApp)))))
-             (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                            (utf8->string body))])
-                 (error (assoc-ref bodySCM "error"))
-               (generate-masto-filter bodySCM)))])))
+     [else (generate-masto-filter
+             (http 'post
+               (string-append (masto-app-domain mastoApp) "/api/v1/filters/"
+                              (masto-filter-id         f))
+               #:token       (masto-app-token mastoApp)
+               #:queryParams `(("phrase"       ,(masto-filter-phrase f))
+                               ("context"      ,(enum-member-or-value->string
+                                                  (masto-filter-context f)))
+                               ("irreversible" ,(boolean->string
+                                                  (masto-filter-irreversible f)))
+                               ("whole_word"   ,(boolean->string
+                                                  (masto-filter-whole-word   f)))
+                               ("expires_in"   ,(if-let ([ei not (masto-filter-expires-at f)])
+                                                    ei
+                                                  (number->string
+                                                    (cond
+                                                     [(date? ei)
+                                                           (time-second
+                                                             (time-difference
+                                                               (date->time-utc ei)
+                                                               (date->time-utc (current-date
+                                                                                 (date-zone-offset ei)))))]
+                                                     [(time? ei)
+                                                           (time-second ei)]
+                                                     [(number? ei)
+                                                           ei]
+                                                     [else (error (string-append
+                                                                    "ERROR: In procedure masto-filter-update:\n"
+                                                                    "In procedure masto-filter-update: "
+                                                                    "expiresIn must be srfi-19 date or time "
+                                                                    "or number of seconds"))])))))))])))
 
 (define (masto-filter-delete mastoApp filterID)
-  (receive (header body)
-      (http-delete
-        (string-append (masto-app-domain mastoApp) "/api/v1/filters/" filterID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'delete
+    (string-append (masto-app-domain mastoApp) "/api/v1/filters/" filterID)
+    #:token (masto-app-token mastoApp))
+
+  #t)

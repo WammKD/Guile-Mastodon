c1db074a994bf2c6ff62f34c02e3e98bbd1f3acc
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 21:22:22 -0600

    Update emojis…

--- emojis.scm
+++ emojis.scm
@@ -1,19 +1,15 @@
 (define-module (elefan emojis)
   #:use-module (elefan auth)
   #:use-module (elefan entities)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (web client)
+  #:use-module (elefan utils)
   #:export (masto-emojis-on-instance))
 
 (define (masto-emojis-on-instance domainOrApp)
-  (receive (header body)
-      (http-get (string-append
-                  (if (masto-instance-app? domainOrApp)
-                      (masto-app-domain domainOrApp)
-                    (if (string-contains-ci domainOrApp "https://")
-                        domainOrApp
-                      (string-append/shared "https://" domainOrApp)))
-                  "/api/v1/custom_emojis"))
-    (generate-masto-emoji-array (json-string->scm (utf8->string body)))))
+  (generate-masto-emoji-array
+    (http 'get (string-append
+                 (if (masto-instance-app? domainOrApp)
+                     (masto-app-domain domainOrApp)
+                   (if (string-contains-ci domainOrApp "https://")
+                       domainOrApp
+                     (string-append/shared "https://" domainOrApp)))
+                 "/api/v1/custom_emojis"))))

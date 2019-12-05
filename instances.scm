0fae715560b490d172ee2f8efff76349091d5fcb
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 22:38:42 -0600

    Update instances…

--- instances.scm
+++ instances.scm
@@ -1,19 +1,15 @@
 (define-module (elefan instances)
   #:use-module (elefan auth)
   #:use-module (elefan entities)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (web client)
+  #:use-module (elefan utils)
   #:export (masto-instance-info))
 
 (define (masto-instance-info domainOrApp)
-  (receive (header body)
-      (http-get (string-append
-                  (if (masto-instance-app? domainOrApp)
-                      (masto-app-domain domainOrApp)
-                    (if (string-contains-ci domainOrApp "https://")
-                        domainOrApp
-                      (string-append/shared "https://" domainOrApp)))
-                  "/api/v1/instance"))
-    (generate-masto-instance (json-string->scm (utf8->string body)))))
+  (generate-masto-instance
+    (http 'get (string-append
+                 (if (masto-instance-app? domainOrApp)
+                     (masto-app-domain domainOrApp)
+                   (if (string-contains-ci domainOrApp "https://")
+                       domainOrApp
+                     (string-append/shared "https://" domainOrApp)))
+                 "/api/v1/instance"))))

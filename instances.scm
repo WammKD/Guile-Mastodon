56a13e3f8a3bb7afb0d37b7aa5ca9ef9662313c1
Sherlock Holmes <jaft.r@outlook.com>
2019-12-05 14:01:03 -0600

    And there we go; besides `instance` URLs and stat.s, the Contact Account is an `account` record object so let's re-export those bits, as well.

--- instances.scm
+++ instances.scm
@@ -2,7 +2,48 @@
   #:use-module (elefan auth)
   #:use-module (elefan entities)
   #:use-module (elefan utils)
-  #:export (masto-instance-info))
+  #:export (masto-instance-info)
+  #:re-export (<mastodon-emoji>   masto-emoji?   masto-emoji-shortcode
+                                                 masto-emoji-static-url
+                                                 masto-emoji-url
+                                                 masto-emoji-visible-in-picker
+               <mastodon-field>   masto-field?   masto-field-name
+                                                 masto-field-value
+                                                 masto-field-verified-at
+               <mastodon-account> masto-account? masto-account-id
+                                                 masto-account-username
+                                                 masto-account-acct
+                                                 masto-account-display-name
+                                                 masto-account-locked
+                                                 masto-account-created-at
+                                                 masto-account-followers-count
+                                                 masto-account-following-count
+                                                 masto-account-statuses-count
+                                                 masto-account-note
+                                                 masto-account-url
+                                                 masto-account-avatar
+                                                 masto-account-avatar-static
+                                                 masto-account-header
+                                                 masto-account-header-static
+                                                 masto-account-emojis
+                                                 masto-account-moved
+                                                 masto-account-fields
+                                                 masto-account-bot
+               <mastodon-instance-urls>  masto-instance-urls?  masto-instance-urls-streaming-api
+               <mastodon-instance-stats> masto-instance-stats? masto-instance-stats-user-count
+                                                               masto-instance-stats-status-count
+                                                               masto-instance-stats-domain-count
+               <mastodon-instance>       masto-instance?       masto-instance-uri
+                                                               masto-instance-title
+                                                               masto-instance-short-description
+                                                               masto-instance-description
+                                                               masto-instance-email
+                                                               masto-instance-version
+                                                               masto-instance-thumbnail
+                                                               masto-instance-urls
+                                                               masto-instance-stats
+                                                               masto-instance-languages
+                                                               masto-instance-contact-account))
 
 (define (masto-instance-info domainOrApp)
   (generate-masto-instance

a5c307d063425a803609fb86a6e20ab85aeab151
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 23:20:23 -0600

    Update scheduled statuses…

--- scheduled-statuses.scm
+++ scheduled-statuses.scm
@@ -2,41 +2,25 @@
   #:use-module (elefan auth)
   #:use-module (elefan entities)
   #:use-module (elefan utils)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (web client)
-  #:export (masto-scheduled-statuses-all
-            masto-scheduled-status-get
-            masto-scheduled-status-update
-            masto-scheduled-status-delete))
+  #:use-module (srfi srfi-19)
+  #:export (masto-scheduled-statuses-all  masto-scheduled-status-get
+            masto-scheduled-status-update masto-scheduled-status-delete))
 
 (define (masto-scheduled-statuses-all mastoApp)
-  (receive (header body)
-      (http-get
-        (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-scheduled-status-array bodySCM))))
+  (generate-masto-scheduled-status-array
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses")
+      #:token (masto-app-token mastoApp))))
 
 (define (masto-scheduled-status-get mastoApp scheduledStatusID)
-  (receive (header body)
-      (http-get
-        (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses/"
-                       scheduledStatusID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-scheduled-status bodySCM))))
+  (generate-masto-scheduled-status
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses/"
+                     scheduledStatusID)
+      #:token (masto-app-token mastoApp))))
 
-(define* (masto-scheduled-status-update mastoApp #:key scheduledStatusID scheduledAt scheduledStatus)
+(define* (masto-scheduled-status-update mastoApp #:key scheduledStatus
+                                                       scheduledStatusID scheduledAt)
   (if (and (not scheduledStatus) (not (and scheduledStatusID scheduledAt)))
       (error (string-append
                "ERROR: In procedure masto-scheduled-status-update:\n"
@@ -51,32 +35,21 @@
           [finalID          (if scheduledStatus
                                 (masto-scheduled-status-id scheduledStatus)
                               scheduledStatusID)])
-      (receive (header body)
-          (http-put
-            (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses/"
-                           finalID
-                           (assemble-params
-                             `(("scheduled_at" ,(if (string? finalScheduledAt)
-                                                    finalScheduledAt
-                                                  (date->string
-                                                    finalScheduledAt
-                                                    "~Y-~m-~dT~H:~M:~S.~N~z"))))))
-            #:headers `((Authorization . ,(string-append
-                                            "Bearer "
-                                            (masto-app-token mastoApp)))))
-        (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                       (utf8->string body))])
-            (error (assoc-ref bodySCM "error"))
-          (generate-masto-scheduled-status bodySCM))))))
+      (generate-masto-scheduled-status
+        (http 'put
+          (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses/"
+                         finalID)
+          #:token       (masto-app-token mastoApp)
+          #:queryParams `(("scheduled_at" ,(if (string? finalScheduledAt)
+                                               finalScheduledAt
+                                             (date->string
+                                               finalScheduledAt
+                                               "~Y-~m-~dT~H:~M:~S.~N~z")))))))))
 
 (define (masto-scheduled-status-delete mastoApp scheduledStatusID)
-  (receive (header body)
-      (http-delete
-        (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses/"
-                       scheduledStatusID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'delete
+    (string-append (masto-app-domain mastoApp) "/api/v1/scheduled_statuses/"
+                   scheduledStatusID)
+    #:token (masto-app-token mastoApp))
+
+  #t)

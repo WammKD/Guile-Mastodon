7d844cddfaa077cc6342f4baf2772f3a1ecd3713
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 21:28:19 -0600

    Update endorsements…

--- endorsements.scm
+++ endorsements.scm
@@ -1,10 +1,7 @@
 (define-module (elefan endorsements)
   #:use-module (elefan auth)
   #:use-module (elefan entities)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (web client)
+  #:use-module (elefan utils)
   #:export (masto-endorsements-all
             masto-endorse-account
             masto-unendorse-account))
@@ -12,27 +9,21 @@
 (define* (masto-endorsements-all mastoApp #:optional [limit 40])
   (generate-masto-page
     mastoApp
-    http-get
+    'get
     (string-append (masto-app-domain mastoApp) "/api/v1/endorsements"
                    "?limit="                   (number->string limit))
     generate-masto-account-array))
     
 (define (masto-endorse-account mastoApp accountID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
-                       accountID                   "/pin")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (generate-masto-relationship (json-string->scm (utf8->string body)))))
+  (generate-masto-relationship
+    (http 'post
+      (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
+                     accountID                   "/pin")
+      #:token (masto-app-token mastoApp))))
 
 (define (masto-unendorse-account mastoApp accountID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
-                       accountID                   "/unpin")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (generate-masto-relationship (json-string->scm (utf8->string body)))))
+  (generate-masto-relationship
+    (http 'post
+      (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
+                     accountID                   "/unpin")
+      #:token (masto-app-token mastoApp))))

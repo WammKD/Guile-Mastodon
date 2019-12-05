1d4762f290151bf24d3487202e795a8f2c5e1801
Sherlock Holmes <jaft.r@outlook.com>
2019-12-05 01:54:42 -0600

    On to the next one…

--- follow-requests.scm
+++ follow-requests.scm
@@ -4,7 +4,36 @@
   #:use-module (elefan utils)
   #:export (masto-follow-requests-all
             masto-follow-request-authorize
-            masto-follow-request-reject))
+            masto-follow-request-reject)
+  #:re-export (<mastodon-pagination-object> masto-page? masto-page-objects
+                                                        masto-page-prev
+                                                        masto-page-next
+               <mastodon-emoji>   masto-emoji?   masto-emoji-shortcode
+                                                 masto-emoji-static-url
+                                                 masto-emoji-url
+                                                 masto-emoji-visible-in-picker
+               <mastodon-field>   masto-field?   masto-field-name
+                                                 masto-field-value
+                                                 masto-field-verified-at
+               <mastodon-account> masto-account? masto-account-id
+                                                 masto-account-username
+                                                 masto-account-acct
+                                                 masto-account-display-name
+                                                 masto-account-locked
+                                                 masto-account-created-at
+                                                 masto-account-followers-count
+                                                 masto-account-following-count
+                                                 masto-account-statuses-count
+                                                 masto-account-note
+                                                 masto-account-url
+                                                 masto-account-avatar
+                                                 masto-account-avatar-static
+                                                 masto-account-header
+                                                 masto-account-header-static
+                                                 masto-account-emojis
+                                                 masto-account-moved
+                                                 masto-account-fields
+                                                 masto-account-bot))
 
 (define* (masto-follow-requests-all mastoApp #:optional [limit 40])
   (generate-masto-page

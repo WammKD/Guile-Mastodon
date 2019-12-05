14213f9e2f16e78b9f2f00c6d19825ade14c14eb
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 22:37:03 -0600

    Update follow suggestions…

--- follow-suggestions.scm
+++ follow-suggestions.scm
@@ -2,32 +2,18 @@
   #:use-module (elefan auth)
   #:use-module (elefan entities)
   #:use-module (elefan utils)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (srfi srfi-26)
-  #:use-module (web client)
-  #:export ())
+  #:export (masto-follow-suggestions-all
+            masto-follow-suggestion-delete))
 
 (define (masto-follow-suggestions-all mastoApp)
-  (receive (header body)
-      (http-get
-        (string-append (masto-app-domain mastoApp) "/api/v1/suggestions")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-account-array bodySCM))))
+  (generate-masto-account-array
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/suggestions")
+      #:token (masto-app-token mastoApp))))
 
 (define (masto-follow-suggestion-delete mastoApp accountID)
-  (receive (header body)
-      (http-delete
-        (string-append (masto-app-domain mastoApp) "/api/v1/suggestions/" accountID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'delete
+    (string-append (masto-app-domain mastoApp) "/api/v1/suggestions/" accountID)
+    #:token (masto-app-token mastoApp))
+
+  #t)

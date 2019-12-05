9dfe39ef134b1e4eef5269e0ff57a7af11f0cdf2
Sherlock Holmes <jaft.r@outlook.com>
2019-12-05 01:58:38 -0600

    And on to the next one…

--- follow-suggestions.scm
+++ follow-suggestions.scm
@@ -3,7 +3,33 @@
   #:use-module (elefan entities)
   #:use-module (elefan utils)
   #:export (masto-follow-suggestions-all
-            masto-follow-suggestion-delete))
+            masto-follow-suggestion-delete)
+  #:re-export (<mastodon-emoji>   masto-emoji?   masto-emoji-shortcode
+                                                 masto-emoji-static-url
+                                                 masto-emoji-url
+                                                 masto-emoji-visible-in-picker
+               <mastodon-field>   masto-field?   masto-field-name
+                                                 masto-field-value
+                                                 masto-field-verified-at
+               <mastodon-account> masto-account? masto-account-id
+                                                 masto-account-username
+                                                 masto-account-acct
+                                                 masto-account-display-name
+                                                 masto-account-locked
+                                                 masto-account-created-at
+                                                 masto-account-followers-count
+                                                 masto-account-following-count
+                                                 masto-account-statuses-count
+                                                 masto-account-note
+                                                 masto-account-url
+                                                 masto-account-avatar
+                                                 masto-account-avatar-static
+                                                 masto-account-header
+                                                 masto-account-header-static
+                                                 masto-account-emojis
+                                                 masto-account-moved
+                                                 masto-account-fields
+                                                 masto-account-bot))
 
 (define (masto-follow-suggestions-all mastoApp)
   (generate-masto-account-array

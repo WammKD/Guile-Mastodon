7cb1ff54ed4b5c12a77627dcf83de16bda835099
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 21:19:16 -0600

    And now adjust the domain-blocks file…

--- domain-blocks.scm
+++ domain-blocks.scm
@@ -2,10 +2,6 @@
   #:use-module (elefan auth)
   #:use-module (elefan entities)
   #:use-module (elefan utils)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (web client)
   #:export (masto-domain-blocks-all
             masto-block-domain
             masto-unblock-domain))
@@ -13,31 +9,23 @@
 (define* (masto-domain-blocks-all mastoApp #:optional [limit 40])
   (generate-masto-page
     mastoApp
-    http-get
+    'get
     (string-append (masto-app-domain mastoApp) "/api/v1/domain_blocks"
                    "?limit="                   (number->string limit))
     vector->list))
 
 (define (masto-block-domain mastoApp domain)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/domain_blocks"
-                       "?domain="                  domain)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'post
+    (string-append (masto-app-domain mastoApp) "/api/v1/domain_blocks")
+    #:token       (masto-app-token mastoApp)
+    #:queryParams `(("domain" ,domain)))
+
+  #t)
 
 (define (masto-unblock-domain mastoApp domain)
-  (receive (header body)
-      (http-delete
-        (string-append (masto-app-domain mastoApp) "/api/v1/domain_blocks"
-                       "?domain="                  domain)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'delete
+    (string-append (masto-app-domain mastoApp) "/api/v1/domain_blocks")
+    #:token       (masto-app-token mastoApp)
+    #:queryParams `(("domain" ,domain)))
+
+  #t)

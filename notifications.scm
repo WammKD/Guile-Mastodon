70b364795e3b5dd357cb43648da5d6b4cc547a23
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 23:10:40 -0600

    Update notifications…

--- notifications.scm
+++ notifications.scm
@@ -3,27 +3,21 @@
   #:use-module (elefan entities)
   #:use-module (elefan enums)
   #:use-module (elefan utils)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-26)
-  #:use-module (web client)
   #:use-module (web uri)
-  #:export (masto-notifications-all
-            masto-notification-get
-            masto-notifications-all-clear
-            masto-notification-delete
-            masto-web-push-get-subscription
-            masto-web-push-update-subscription
+  #:export (masto-notifications-all            masto-notification-get
+            masto-notifications-all-clear      masto-notification-delete
+            masto-web-push-get-subscription    masto-web-push-update-subscription
             masto-web-push-delete-subscription))
 
-(define* (masto-notifications-all mastoApp #:key maxID      sinceID      minID
-                                                 [limit 20] excludeTypes accountID)
+(define* (masto-notifications-all mastoApp #:key maxID              sinceID
+                                                 minID              [limit 20]
+                                                 [excludeTypes '()] accountID)
   (if (every (cut enum-member? <> NOTIFICATION_TYPE_ENUM) excludeTypes)
       (generate-masto-page
         mastoApp
-        http-get
+        'get
         (string-append (masto-app-domain mastoApp) "/api/v1/notifications"
                        (assemble-params
                          `(("max_id"        ,maxID)
@@ -43,40 +37,26 @@
              "reblog, or favourite"))))
 
 (define (masto-notification-get mastoApp notificationID)
-  (receive (header body)
-      (http-get
-        (string-append (masto-app-domain mastoApp) "/api/v1/notifications/"
-                       notificationID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-notification bodySCM))))
+  (generate-masto-notification
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/notifications/"
+                     notificationID)
+      #:token (masto-app-token mastoApp))))
 
 (define (masto-notifications-all-clear mastoApp)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/notifications/clear")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'post
+    (string-append (masto-app-domain mastoApp) "/api/v1/notifications/clear")
+    #:token (masto-app-token mastoApp))
+
+  #t)
 
 (define (masto-notification-delete mastoApp notificationID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/notifications/dismiss"
-                       "?id="                      notificationID)
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'post
+    (string-append (masto-app-domain mastoApp) "/api/v1/notifications/dismiss")
+    #:token       (masto-app-token mastoApp)
+    #:queryParams `(("id" ,notificationID)))
+
+  #t)
 
 (define* (masto-web-push-create-subscription mastoApp               subscriptionEndpoint
                                              subscriptionKeysP256dh subscriptionKeysAuth
@@ -85,90 +65,46 @@
                                                                     dataAlertsReblog
                                                                     dataAlertsMention
                                                                     dataAlertsPoll)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription"
-                       (assemble-params
-                         `(("subscription[endpoint]"     ,(if (uri? subscriptionEndpoint)
-                                                              (uri->string
-                                                                subscriptionEndpoint)
-                                                            subscriptionEndpoint))
-                           ("subscription[keys][p256dh]" ,subscriptionKeysP256dh)
-                           ("subscription[keys][auth]"   ,subscriptionKeysAuth)
-                           ("data[alerts][follow]"       ,(if dataAlertsFollow
-                                                              "true"
-                                                            "false"))
-                           ("data[alerts][favourite]"    ,(if dataAlertsFavorite
-                                                              "true"
-                                                            "false"))
-                           ("data[alerts][reblog]"       ,(if dataAlertsReblog
-                                                              "true"
-                                                            "false"))
-                           ("data[alerts][mention]"      ,(if dataAlertsMention
-                                                              "true"
-                                                            "false"))
-                           ("data[alerts][poll]"         ,(if dataAlertsPoll
-                                                              "true"
-                                                            "false")))))
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-web-push-subscription bodySCM))))
+  (generate-masto-web-push-subscription
+    (http 'post
+      (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription")
+      #:token       (masto-app-token mastoApp)
+      #:queryParams `(("subscription[endpoint]"     ,(if (uri? subscriptionEndpoint)
+                                                         (uri->string
+                                                           subscriptionEndpoint)
+                                                       subscriptionEndpoint))
+                      ("subscription[keys][p256dh]" ,subscriptionKeysP256dh)
+                      ("subscription[keys][auth]"   ,subscriptionKeysAuth)
+                      ("data[alerts][follow]"       ,(boolean->string dataAlertsFollow))
+                      ("data[alerts][favourite]"    ,(boolean->string dataAlertsFavorite))
+                      ("data[alerts][reblog]"       ,(boolean->string dataAlertsReblog))
+                      ("data[alerts][mention]"      ,(boolean->string dataAlertsMention))
+                      ("data[alerts][poll]"         ,(boolean->string dataAlertsPoll))))))
 
 (define (masto-web-push-get-subscription mastoApp)
-  (receive (header body)
-      (http-get
-        (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-web-push-subscription bodySCM))))
+  (generate-masto-web-push-subscription
+    (http 'get
+      (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription")
+      #:token (masto-app-token mastoApp))))
 
 (define* (masto-web-push-update-subscription mastoApp #:key dataAlertsFollow
                                                             dataAlertsFavorite
                                                             dataAlertsReblog
                                                             dataAlertsMention
                                                             dataAlertsPoll)
-  (receive (header body)
-      (http-put
-        (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription"
-                       (assemble-params
-                         `(("data[alerts][follow]"    ,(if dataAlertsFollow
-                                                           "true"
-                                                         "false"))
-                           ("data[alerts][favourite]" ,(if dataAlertsFavorite
-                                                           "true"
-                                                         "false"))
-                           ("data[alerts][reblog]"    ,(if dataAlertsReblog
-                                                           "true"
-                                                         "false"))
-                           ("data[alerts][mention]"   ,(if dataAlertsMention
-                                                           "true"
-                                                         "false"))
-                           ("data[alerts][poll]"      ,(if dataAlertsPoll
-                                                           "true"
-                                                         "false")))))
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([bodySCM (cut assoc-ref <> "error") (json-string->scm
-                                                   (utf8->string body))])
-        (error (assoc-ref bodySCM "error"))
-      (generate-masto-web-push-subscription bodySCM))))
+  (generate-masto-web-push-subscription
+    (http 'put
+      (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription")
+      #:token       (masto-app-token mastoApp)
+      #:queryParams `(("data[alerts][follow]"    ,(boolean->string dataAlertsFollow))
+                      ("data[alerts][favourite]" ,(boolean->string dataAlertsFavorite))
+                      ("data[alerts][reblog]"    ,(boolean->string dataAlertsReblog))
+                      ("data[alerts][mention]"   ,(boolean->string dataAlertsMention))
+                      ("data[alerts][poll]"      ,(boolean->string dataAlertsPoll))))))
 
 (define (masto-web-push-delete-subscription mastoApp)
-  (receive (header body)
-      (http-delete
-        (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'delete
+    (string-append (masto-app-domain mastoApp) "/api/v1/push/subscription")
+    #:token (masto-app-token mastoApp))
+
+  #t)

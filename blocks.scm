6a7bee1089434a5888586b45b48fc408938b4a43
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 20:24:27 -0600

    Update the blocks calls.

--- blocks.scm
+++ blocks.scm
@@ -1,9 +1,7 @@
 (define-module (elefan blocks)
   #:use-module (elefan auth)
   #:use-module (elefan entities)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
+  #:use-module (elefan utils)
   #:use-module (web client)
   #:export (masto-blocks-all
             masto-block-account
@@ -18,21 +16,15 @@
     generate-masto-account-array))
 
 (define (masto-block-account mastoApp accountID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
-                       accountID                   "/block")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (generate-masto-relationship (json-string->scm (utf8->string body)))))
+  (generate-masto-relationship
+    (http 'post
+      (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
+                     accountID                   "/block")
+      #:token (masto-app-token mastoApp))))
 
 (define (masto-unblock-account mastoApp accountID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
-                       accountID                   "/unblock")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (generate-masto-relationship (json-string->scm (utf8->string body)))))
+  (generate-masto-relationship
+    (http 'post
+      (string-append (masto-app-domain mastoApp) "/api/v1/accounts/"
+                     accountID                   "/unblock")
+      #:token (masto-app-token mastoApp))))

58b91d385074f9aa1842584618eb8536cd002cec
Sherlock Holmes <jaft.r@outlook.com>
2019-12-04 22:08:40 -0600

    Update follow requests…

--- follow-requests.scm
+++ follow-requests.scm
@@ -1,10 +1,7 @@
 (define-module (elefan follow-requests)
   #:use-module (elefan auth)
   #:use-module (elefan entities)
-  #:use-module (ice-9 receive)
-  #:use-module (json)
-  #:use-module (rnrs bytevectors)
-  #:use-module (web client)
+  #:use-module (elefan utils)
   #:export (masto-follow-requests-all
             masto-follow-request-authorize
             masto-follow-request-reject))
@@ -12,31 +9,23 @@
 (define* (masto-follow-requests-all mastoApp #:optional [limit 40])
   (generate-masto-page
     mastoApp
-    http-get
+    'get
     (string-append (masto-app-domain mastoApp) "/api/v1/follow_requests"
                    "?limit="                   (number->string limit))
     generate-masto-account-array))
 
 (define (masto-follow-request-authorize mastoApp requestID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/follow_requests/"
-                       requestID                   "/authorize")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'post
+    (string-append (masto-app-domain mastoApp) "/api/v1/follow_requests/"
+                   requestID                   "/authorize")
+    #:token (masto-app-token mastoApp))
+
+  #t)
 
 (define (masto-follow-request-reject mastoApp requestID)
-  (receive (header body)
-      (http-post
-        (string-append (masto-app-domain mastoApp) "/api/v1/follow_requests/"
-                       requestID                   "/reject")
-        #:headers `((Authorization . ,(string-append
-                                        "Bearer "
-                                        (masto-app-token mastoApp)))))
-    (if-let ([errMsg (assoc-ref (json-string->scm (utf8->string body)) "error")])
-        (error errMsg)
-      #t)))
+  (http 'post
+    (string-append (masto-app-domain mastoApp) "/api/v1/follow_requests/"
+                   requestID                   "/reject")
+    #:token (masto-app-token mastoApp))
+
+  #t)

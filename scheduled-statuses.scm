8e5cd0f4036f35c1b0bfec1a02f64ac13356d569
Sherlock Holmes <jaft.r@outlook.com>
2019-12-05 22:46:12 -0600

    Just requires a scheduled status, which requires a scheduled status param and media attachment, which requires a meta, which requires a meta focus and meta subtree.

--- scheduled-statuses.scm
+++ scheduled-statuses.scm
@@ -4,7 +4,40 @@
   #:use-module (elefan utils)
   #:use-module (srfi srfi-19)
   #:export (masto-scheduled-statuses-all  masto-scheduled-status-get
-            masto-scheduled-status-update masto-scheduled-status-delete))
+            masto-scheduled-status-update masto-scheduled-status-delete)
+  #:re-export (<mastodon-meta-subtree> masto-meta-subtree? masto-meta-subtree-width
+                                                           masto-meta-subtree-height
+                                                           masto-meta-subtree-size
+                                                           masto-meta-subtree-aspect
+                                                           masto-meta-subtree-frame-rate
+                                                           masto-meta-subtree-duration
+                                                           masto-meta-subtree-bitrate
+               <mastodon-meta-focus>   masto-meta-focus?   masto-meta-focus-x
+                                                           masto-meta-focus-y
+               <mastodon-meta>         masto-meta?         masto-meta-small
+                                                           masto-meta-original
+                                                           masto-meta-focus
+               <mastodon-attachment>   masto-attachment?   masto-attachment-id
+                                                           masto-attachment-type
+                                                           masto-attachment-url
+                                                           masto-attachment-remote-url
+p                                                           masto-attachment-preview-url
+                                                           masto-attachment-text-url
+                                                           masto-attachment-meta
+                                                           masto-attachment-description
+                                                           masto-attachment-blurhash
+               <mastodon-scheduled-status-params> masto-scheduled-status-params? masto-scheduled-status-params-text
+                                                                                 masto-scheduled-status-params-in-reply-to-id
+                                                                                 masto-scheduled-status-params-media-ids
+                                                                                 masto-scheduled-status-params-sensitive
+                                                                                 masto-scheduled-status-params-spoiler-text
+                                                                                 masto-scheduled-status-params-visibility
+                                                                                 masto-scheduled-status-params-scheduled-at
+                                                                                 masto-scheduled-status-params-application-id
+               <mastodon-scheduled-status> masto-scheduled-status? masto-scheduled-status-id
+                                                                   masto-scheduled-status-scheduled-at
+                                                                   masto-scheduled-status-params
+                                                                   masto-scheduled-status-media-attachments))
 
 (define (masto-scheduled-statuses-all mastoApp)
   (generate-masto-scheduled-status-array

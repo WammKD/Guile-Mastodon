b02ed89fd111a8edb80e0847da3d5fc9b5a16b48
Sherlock Holmes <jaft.r@outlook.com>
2020-04-13 02:57:44 -0500

    A script to generate documentation. It's not the best and I feel like I should have better tools than this for a Scheme…but it's sure as Hell better than nothing.
    This way I can provide commentary for modules and documentation in
    docstrings for the functions and this script will be able to grab all
    of that for me without me having to update more than one place.
    
    And, while I'd prefer being able to better capture the nature of, say,
    arguments (like not just which are keyword arguments but if those
    keyword arguments have defaults), that'll get caught by my
    documentation in the docstring, anyway (since I don't believe, for
    example, Geiser ends up specifying whether the arguments have
    defaults, either), and so it's still capturing all the absolutely
    crucial elements, even if it's a bit more work for my end.
    
    The big thing is just not having to update more than one place; that's
    what I wanted the most, in the end.

---
+++ generate-docs.scm
@@ -0,0 +1,139 @@
+;;;;    Copyright (C) 2019,2020
+;;;;
+;;;; This library is free software; you can redistribute it and/or
+;;;; modify it under the terms of the GNU General Public
+;;;; License as published by the Free Software Foundation; either
+;;;; version 3 of the License, or (at your option) any later version.
+;;;;
+;;;; This library is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with this library; if not, write to the Free Software
+;;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+;;;;
+(use-modules (ice-9 documentation)
+             (ice-9 session))
+
+(add-to-load-path ".")
+
+(let ([dir (opendir "docs")])
+  (do ([entry (readdir dir) (readdir dir)])
+      [(eof-object? entry)]
+    (when (not (or (string=? entry ".") (string=? entry "..")))
+      (delete-file (string-append "docs/" entry)))))
+
+(let ([dir (opendir "elefan")])
+  (do ([entry (readdir dir) (readdir dir)])
+      [(eof-object? entry)]
+    (let ([index (string-contains-ci entry ".scm")])
+      (when (and
+              index
+              (= index (- (string-length entry) 4))
+              (not (string-ci=? entry "utils.scm"))
+              (not (string-ci=? entry "enums.scm"))
+              (not (string-ci=? entry "entities.scm")))
+        (let ([moduleName (substring entry 0 index)])
+          (eval-string (string-append "(use-modules (elefan " moduleName "))"))
+
+          (call-with-output-file (string-append "docs/" moduleName ".md")
+            (lambda (outputPort)
+              (define (disp str) (display str outputPort))
+              (define (newln)    (newline     outputPort))
+
+              (disp "# ")
+              (disp moduleName)
+              (disp " Module")
+              (newln)
+
+              (disp (file-commentary (string-append "elefan/" entry)))
+              (newln)
+              (newln)
+              (disp "<br />")
+              (newln)
+              (newln)
+
+              (module-for-each
+                (lambda (sym var)
+                  (disp "## ")
+                  (disp (symbol->string sym))
+                  (newln)
+
+                  (disp "#### Summary")
+                  (newln)
+
+                  (if (eval-string (string-append
+                                     "(record-type? "
+                                     (symbol->string sym)
+                                     ")"))
+                      (begin
+                        (disp "A record object that can be returned by an API call.")
+                        (newln)
+
+                        (disp "#### Record Fields")
+                        (newln)
+
+                        (for-each
+                          (lambda (fieldAsSym)
+                            (disp "> `")
+                            (disp fieldAsSym)
+                            (disp "` <br />")
+                            (newln))
+                          (eval-string (string-append
+                                         "(record-type-fields "
+                                         (symbol->string sym)
+                                         ")"))))
+                    (begin
+                      (disp (eval-string (string-append
+                                           "(procedure-documentation "
+                                           (symbol->string sym)
+                                           ")")))
+                      (newln)
+
+                      (disp "#### Parameters")
+                      (newln)
+
+                      (for-each
+                        (lambda (argAsSym)
+                          (disp "> `")
+                          (disp argAsSym)
+                          (disp "` _required_ <br />")
+                          (newln))
+                        (assoc-ref (eval-string (string-append
+                                                  "(procedure-arguments "
+                                                  (symbol->string sym)
+                                                  ")")) 'required))
+
+                      (for-each
+                        (lambda (keywordPair)
+                          (disp "> `")
+                          (disp (car keywordPair))
+                          (disp "` _keyword_ (argument position ")
+                          (disp (cdr keywordPair))
+                          (disp ") <br />")
+                          (newln))
+                        (assoc-ref (eval-string (string-append
+                                                  "(procedure-arguments "
+                                                  (symbol->string sym)
+                                                  ")")) 'keyword))
+
+                      (for-each
+                        (lambda (optAsSym)
+                          (disp "> `")
+                          (disp optAsSym)
+                          (disp "` _required_ <br />")
+                          (newln))
+                        (assoc-ref (eval-string (string-append
+                                                  "(procedure-arguments "
+                                                  (symbol->string sym)
+                                                  ")")) 'optional))))
+
+                  (newln)
+                  (disp "<br />")
+                  (newln)
+                  (newln))
+                (resolve-interface `(elefan ,(string->symbol moduleName))))))))))
+
+  (closedir dir))

08c07f034524940f792d399793804436b78e9826
Sherlock Holmes <Wamm_KD_Schmelingski@yahoo.com>
2020-09-04 18:56:39 -0500

    The notification types increased so updating this.
    2.9.0 - add `account_id`
    3.1.0 - add `follow_request` type

--- elefan/enums.scm
+++ elefan/enums.scm
@@ -22,8 +22,9 @@
 
 
 
-(define NOTIFICATION_TYPE_ENUM '(follow  mention       reblog  favourite))
-(define   ATTACHMENT_TYPE_ENUM '(unknown image         gifv    video))
-(define         CARD_TYPE_ENUM '(link    photo         video   rich))
-(define    FILTER_CONTEXT_ENUM '(home    notifications public  thread))
-(define STATUS_VISIBILITY_ENUM '(public  unlisted      private direct))
+(define NOTIFICATION_TYPE_ENUM '(follow  favourite      reblog
+                                 poll    follow_request mention))
+(define   ATTACHMENT_TYPE_ENUM '(unknown image          gifv    video))
+(define         CARD_TYPE_ENUM '(link    photo          video   rich))
+(define    FILTER_CONTEXT_ENUM '(home    notifications  public  thread))
+(define STATUS_VISIBILITY_ENUM '(public  unlisted       private direct))
